95d46bb530c68e0dd0fa38f81e782d86c7e3de6d
Abhishek Raj <abhishek.raj.singh27@gmail.com>
2023-09-30 03:25:37 +0530

    Update #2

--- src/assets/experience.js
+++ src/assets/experience.js
@@ -4,21 +4,22 @@ export const experience = [
         company: "Wreken Inc.",
         from: "August 2023",
         to: "Present",
-        description: "....In progress....",
+        description:
+            "Led the creation of a dynamic Svelte-based VSCode plugin, fast-tracking 3rd party code integration. Developed the VSCode Sidebar, finishing a 2-week project ahead of schedule, saving 40% on timelines. Transformed the company website with animations, lazy loading, and three new pages, boosting user engagement and online presence. Streamlined processes for early investor meetings and demos.",
         tags: ["Svelte", "ReactJs", "Typescript", "REST API", "Postman"],
     },
     {
-        designation: "Computer Networks Teaching Assistant",
-        company: "LNMIIT",
-        from: "Jan 2023",
-        to: "April 2023",
+        designation: "SDE Intern",
+        company: "Samsung Electronics",
+        from: "January 2023",
+        to: "July 2023",
         description:
-            "I have assisted ~80 students, and  my role has been centered around supporting and guiding students through their coursework.",
-        tags: ["Omnet++", "Cisco Packet Tracer"],
+            "Enhanced a communication platform for improved internal collaboration. Designed a responsive React search bar for swift user search autocomplete and implemented a dynamic chat loading system, facilitating message retrieval of up to 100 messages. Contributed to a React dashboard project, optimizing intern activity management with a secure login portal. Collaborated closely with the web development team to seamlessly integrate the dashboard with existing systems and databases, enhancing progress tracking efficiency.",
+        tags: ["ReactJS", "Javascript", "MySQL", "MUI"],
     },
     {
         designation: "Full stack developer",
-        company: "LUSIP, LNMIIT",
+        company: "NSUT, Delhi",
         from: "May 2022",
         to: "July 2022",
         description:
--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,34 +1,42 @@
 export const projects = [
     {
-        title: 'CodeCollab',
-        linklink: 'https://nkg-cc.netlify.app/',
-        githubLink: 'https://github.com/nitishgupta08/CodeCollab',
-        imgSrc: '/codecollab.jpg',
-        from: 'March 2023',
-        to: 'April 2023',
-        description: 'An online code editor that allows users to work together on coding projects in a shared space. Implemented several APIs that perform CRUD operations on users and the spaces they own. Users can  choose variety of languages and themes.',
-        tags: ['ReactJS', 'NodeJS', 'MongoDB', 'Socket.io', 'MUI', 'Codemirror']
+        title: "CodeCollab",
+        linklink: "https://nkg-cc.netlify.app/",
+        githubLink: "https://github.com/AbhishekRaj27/CodeCollabHub",
+        imgSrc: "/codecollab.jpg",
+        from: "April 2023",
+        to: "June 2023",
+        description:
+            "An online code editor that allows users to work together on coding projects in a shared space. Implemented several APIs that perform CRUD operations on users and the spaces they own. Users can  choose variety of languages and themes.",
+        tags: [
+            "ReactJS",
+            "NodeJS",
+            "MongoDB",
+            "Socket.io",
+            "MUI",
+            "Codemirror",
+        ],
     },
     {
-        title: 'algotradersonline',
+        title: "algotradersonline",
         linklink: null,
-        githubLink: 'https://github.com/nitishgupta08/algotradersonline',
-        imgSrc: '/algotraders.png',
-        from: 'Jan 2022',
-        to: 'Sept 2022',
-        description: 'Implemented a wide range of account management, paper trading, and options chain features, as well as a homepage, login/registration screen, dashboard, and options chain. Wrote several APIs to retrieve data from MySQL after processing it for use in strategy execution, options trading, and paper trading, taking into account the specific needs of the business.',
-        tags: ['ReactJS', 'Django', 'MySQL', 'Python', 'Apache']
-
+        githubLink: "https://github.com/nitishgupta08/algotradersonline",
+        imgSrc: "/algotraders.png",
+        from: "Jan 2022",
+        to: "Sept 2022",
+        description:
+            "Implemented a wide range of account management, paper trading, and options chain features, as well as a homepage, login/registration screen, dashboard, and options chain. Wrote several APIs to retrieve data from MySQL after processing it for use in strategy execution, options trading, and paper trading, taking into account the specific needs of the business.",
+        tags: ["ReactJS", "Django", "MySQL", "Python", "Apache"],
     },
     {
-        title: 'Sorting Visualizer',
-        linklink: 'https://ng-sorting-visualizer.netlify.app/',
-        githubLink: 'https://github.com/nitishgupta08/Sorting-Visualizer',
-        imgSrc: '/sorting.png',
-        from: 'July 2021',
-        to: 'Aug 2021',
-        description: 'This website visualizes various sorting techniques like bubble sort, quick sort, insertion sort, selection sort, merge sort, shell sort and heap sort. Sorting animation speed can be changed while the array is sorted. You can generate arrays ranging from size 2 to 100 and randomize them. This site is also responsive.',
-        tags: ['HTML', 'CSS', 'Javascript']
-
-    }
-]
+        title: "Sorting Visualizer",
+        linklink: "https://ng-sorting-visualizer.netlify.app/",
+        githubLink: "https://github.com/nitishgupta08/Sorting-Visualizer",
+        imgSrc: "/sorting.png",
+        from: "July 2021",
+        to: "Aug 2021",
+        description:
+            "This website visualizes various sorting techniques like bubble sort, quick sort, insertion sort, selection sort, merge sort, shell sort and heap sort. Sorting animation speed can be changed while the array is sorted. You can generate arrays ranging from size 2 to 100 and randomize them. This site is also responsive.",
+        tags: ["HTML", "CSS", "Javascript"],
+    },
+];
