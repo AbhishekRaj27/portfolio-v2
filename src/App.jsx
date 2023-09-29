90960bece68646c0ce93ee7a3c5c55e8d1430682
Abhishek Raj <abhishek.raj.singh27@gmail.com>
2023-09-30 01:08:51 +0530

    revert ReactGA

--- src/App.jsx
+++ src/App.jsx
@@ -6,41 +6,41 @@ import Contacts from "./screens/Contact";
 import Footer from "./screens/Footer";
 import { Routes, Route } from "react-router-dom";
 import ProjectArchive from "./screens/ProjectArchive";
-import ReactGA from "react-ga4";
+// import ReactGA from "react-ga4";
 
 function App() {
-  ReactGA.initialize(import.meta.env.VITE_GA4);
-  ReactGA.send({
-    hitType: "pageview",
-    page: "/",
-    title: "Landing Page",
-  });
-  ReactGA.send({
-    hitType: "pageview",
-    page: "/project-archive",
-    title: "Project Archive Page",
-  });
+    // ReactGA.initialize(import.meta.env.VITE_GA4);
+    // ReactGA.send({
+    //   hitType: "pageview",
+    //   page: "/",
+    //   title: "Landing Page",
+    // });
+    // ReactGA.send({
+    //   hitType: "pageview",
+    //   page: "/project-archive",
+    //   title: "Project Archive Page",
+    // });
 
-  return (
-    <div className="bg-bgDark">
-      <Routes>
-        <Route
-          path=""
-          element={
-            <main>
-              <Navbar />
-              <Hero />
-              <About />
-              <Projects />
-              <Contacts />
-              <Footer />
-            </main>
-          }
-        />
-        <Route path="project-archive" element={<ProjectArchive />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="bg-bgDark">
+            <Routes>
+                <Route
+                    path=""
+                    element={
+                        <main>
+                            <Navbar />
+                            <Hero />
+                            <About />
+                            <Projects />
+                            <Contacts />
+                            <Footer />
+                        </main>
+                    }
+                />
+                <Route path="project-archive" element={<ProjectArchive />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
